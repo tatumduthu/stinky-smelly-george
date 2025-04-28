7d07ca0c961092d3ec3132cfe967a226df9eea76
tatumduthu <tatumrduthu@gmail.com>
2025-04-27 20:33:38 -0500

    Update table.js

--- scripts/table.js
+++ scripts/table.js
@@ -26,3 +26,18 @@ $(document).ready(() => {
     $("#parkingForm")[0].reset();
   });
 });
+
+
+
+
+
+// w3s: https://www.w3schools.com/howto/howto_js_mobile_navbar.asp
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
